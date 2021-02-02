01eb2aaa1085b0bb4da39cbb290bd5109a0c67d3
William David Lopez Sanchez <wdavidlopez4@gmail.com>
2021-02-02 12:50:04 -0500

    creacion del controlador para crear productos

---
+++ apps/Quality.WebPresentation/Controllers/ProductoController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Quality.Application.Dtos;
+using Quality.Application.Facude;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quality.WebPresentation.Controllers
+{
+    public class ProductoController : Controller
+    {
+        //servicios del sistema encapsolados en  una fachada 
+        private readonly IFacude facude;
+
+        public ProductoController(IFacude facude)
+        {
+            this.facude = facude;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //contralador totalmente desacoplado 
+        public async Task<IActionResult> CrearProducto(ProductoCreacionDto productoCreacionDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var rest = await facude.CrearProducto(productoCreacionDto);
+                return View(rest);
+            }
+            else
+            {
+                return BadRequest("el usuario no es valido");
+            }
+        }
+    }
+}
