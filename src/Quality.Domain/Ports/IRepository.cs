672de8c15353265690da19ba5f1dbc4dfecc3960
William David Lopez Sanchez <wdavidlopez4@gmail.com>
2021-02-02 10:45:22 -0500

    creacion del puesto para el repositorio en el dominio

---
+++ src/Quality.Domain/Ports/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using Quality.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quality.Domain.Ports
+{
+    public interface IRepository
+    {
+        public Task<T> Guardar<T>(T obj) where T : EntityBase;
+
+        public Task<T> Obtener<T>(string id) where T : EntityBase;
+
+        public Task<T> Obtener<T>(Expression<Func<T, bool>> expression) where T : EntityBase;
+
+        public Task<bool> Existe<T>(string id) where T : EntityBase;
+
+        public Task<bool> Existe<T>(Expression<Func<T, bool>> expression) where T : EntityBase;
+    }
+}
